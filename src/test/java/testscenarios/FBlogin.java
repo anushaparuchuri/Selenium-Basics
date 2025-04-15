dc72911aa49e3ca62c4d2db977437a97be6d0e3c
“seenu4selenium” <seenu4selenium@gmail.com>
2025-04-14 18:48:44 -0600

    Fb login prgram developed

--- src/test/java/testscenarios/FBlogin.java
+++ src/test/java/testscenarios/FBlogin.java
@@ -1,10 +1,32 @@
 package testscenarios;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FBlogin {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//Login to FB application
+//		0. Create an webdriver object
+		WebDriver driver;
+//		1. Open any browser
+		driver = new ChromeDriver();
+		
+		//WebDriver driver = new ChromeDriver();
+		
+//		2. Type URL of FB application
+		driver.get("https://www.facebook.com/");
+		
+//		3. Type invalid credentials
+		//selenium will find the object by using any Webdriver Locators
+		driver.findElement(By.name("email")).sendKeys("AnushaTest@gmail.sdgsom");
+		driver.findElement(By.id("pass")).sendKeys("testsdgsdgdsg");
+		
+//		4. Click on login button
+		driver.findElement(By.name("login")).click();
+		
 	}
 
 }
